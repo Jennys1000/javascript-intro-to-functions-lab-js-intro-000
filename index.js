39eef4383b31c7fe1ceb1cf5ca9651ebcfca477a
Jennifer Sanders <jennys1000@gmail.com>
2020-11-04 01:44:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
 
-   else if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+   if (string.toUpperCase() === string) {
+    return "YES INDEED!";
   }
 
-  else if (string === "I love you, Grandma.") {
-    return "I love you, too."
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."};
   }
